3b13251b4c5247e92a1bd7fc416729795ea46ef6
TheKingofDJ <djspam3@web.de>
2025-07-01 16:33:41 +0200

    Google Maps Standort eingefügt

--- first_vite_project/src/components/AddressBox.jsx
+++ first_vite_project/src/components/AddressBox.jsx
@@ -1,7 +1,18 @@
 const AddressBox = () => (
   <div>
     <h3>Adresse</h3>
-    <p>Elzer Str. 2, 65556 Limburg an der Lahn</p>
+    <p>Elzer Str. 2, 65556 Limburg an der Lahn</p><br></br><br></br>
+    <iframe
+      title="Google Maps Standort"
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1543.1000749797718!2d8.051038696833988!3d50.40132371375964!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bdcd332dc91be1%3A0x70c9b2eb480fd818!2sRestaurant%20Schnitzel&#39;s!5e1!3m2!1sde!2sde!4v1751374859831!5m2!1sde!2sde"
+      width="100%"
+      height="250"
+      style={{ border: 2, borderColor: 'black', borderStyle: 'solid' }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+    
   </div>
 );
 
