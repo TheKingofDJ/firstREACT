77bf4c49de85565af635c3b50edc1ec5746a7484
TheKingofDJ <djspam3@web.de>
2025-07-01 10:25:14 +0200

    clearing files

---
+++ first_vite_project/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //Jedes Return-Statement in einer Funktion muss und darf nur einen Wert zurückgeben.
+  //<></>, auch bekannt als Fragment, ist eine Möglichkeit, mehrere Elemente zu gruppieren, ohne ein zusätzliches DOM-Element zu erzeugen.
+  return (<></>)
+}
+
+export default App
